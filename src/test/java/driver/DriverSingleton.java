f7c4ac2b7fae709e95f5511a81e2048826b2f7ff
Nadia Sopot <sopotnadja@gmail.com>
2020-01-29 00:18:12 +0300

    try property

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -16,7 +16,7 @@ public class DriverSingleton {
         if (driver == null) {
             switch (System.getProperty("browser", "firefox")) {
                 case "firefox": {
-                    System.setProperty("webdriver.gecko.driver", "C:/gecko/geckodriver.exe");
+                    System.setProperty("webdriver.gecko.driver", "C:\\gecko\\geckodriver.exe");
                     driver = new FirefoxDriver();
                     break;
                 }
